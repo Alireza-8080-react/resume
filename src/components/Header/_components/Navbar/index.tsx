1852e1ff022e97cde65455a8845b91319e07b7c5
Alireza-8080-react <Alirezashahmoradi98@gmail.com>
2023-08-24 01:05:38 +0330

    added more items in Navbar

--- src/components/Header/_components/Navbar/index.tsx
+++ src/components/Header/_components/Navbar/index.tsx
@@ -3,19 +3,19 @@ import { NavbarItem } from "./_components/NavbarItem";
 
 export const Navbar = () => {
   return (
-    <Grid container>
+    <Grid container spacing={6}>
       <Grid item>
-        <NavbarItem title="home" link="/home" />
-      </Grid>
-      {/* <Grid item>
-        <NavbarItem />
+        <NavbarItem title="home" link="/" />
       </Grid>
       <Grid item>
-        <NavbarItem />
+        <NavbarItem title="about" link="/about" />
       </Grid>
       <Grid item>
-        <NavbarItem />
-      </Grid> */}
+        <NavbarItem title="else" link="/else" />
+      </Grid>
+      <Grid item>
+        <NavbarItem title="sth" link="/sth" />
+      </Grid>
     </Grid>
   );
 };
