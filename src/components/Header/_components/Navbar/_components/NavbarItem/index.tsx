3624641482b540403a41dc8865015e7a22ae498b
Alireza-8080-react <Alirezashahmoradi98@gmail.com>
2023-08-24 01:07:05 +0330

    added style to navbar item

--- src/components/Header/_components/Navbar/_components/NavbarItem/index.tsx
+++ src/components/Header/_components/Navbar/_components/NavbarItem/index.tsx
@@ -1,13 +1,23 @@
+"use client";
+
 import Link from "next/link";
 import { NavbarItemProps } from "./models";
 import { Typography } from "@mui/material";
 
 import classes from "./index.module.scss";
+import { usePathname } from "next/navigation";
 
 export const NavbarItem = ({ title, link }: NavbarItemProps) => {
+  const pathName = usePathname();
+
   return (
     <Link href={link} className={classes.link}>
-      <Typography variant="h5">{title}</Typography>
+      <Typography
+        variant="h5"
+        className={`${pathName === link ? classes.active : ""} ${classes.text}`}
+      >
+        {title}
+      </Typography>
     </Link>
   );
 };
