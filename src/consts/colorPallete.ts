6369d612a1ebba553ba6a6bf8a86df65c13c74a5
Alireza <alirezashahmoradi98@gmail.com>
2023-08-23 20:20:50 +0330

    create color pallete

---
+++ src/consts/colorPallete.ts
@@ -0,0 +1,4 @@
+export const COLOR_PALETTE_NOMAD = "#bbb6a5";
+export const COLOR_PALETTE_SHARK = "#2a2f33";
+export const COLOR_PALETTE_MANTLE = "#8c9491";
+export const COLOR_PALETTE_MANATEE = "#8c8c9c";
