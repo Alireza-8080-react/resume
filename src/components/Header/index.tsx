f9da64b09acbe2976101bda09b2785a1f392833d
Alireza-8080-react <Alirezashahmoradi98@gmail.com>
2023-08-24 02:08:56 +0330

    define more sizes for header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,10 +5,10 @@ import { Navbar } from "./_components/Navbar";
 export const Header = () => {
   return (
     <Grid container>
-      <Grid item xl={4} lg={4} md={6} sm={5}>
+      <Grid item xl={5} lg={5} md={6} sm={5}>
         <Logo />
       </Grid>
-      <Grid item xl={8} lg={8} md={6} sm={7}>
+      <Grid item xl={7} lg={7} md={6} sm={7}>
         <Navbar />
       </Grid>
     </Grid>
