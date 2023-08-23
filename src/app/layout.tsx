30b7562b90cc354845eff58b7e6a2638825233e7
Alireza <alirezashahmoradi98@gmail.com>
2023-08-23 20:20:29 +0330

    apply theme to project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,24 @@
-import './globals.css';
+import { ThemeProvider } from "@mui/material";
+import "./globals.css";
 
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import { ReactNode } from 'react';
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { ReactNode } from "react";
+import { applicationTheme } from "@/theme";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  description: 'A template to create other projects based on',
-  title: 'Template App'
+  description: "A template to create other projects based on",
+  title: "Template App",
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang='en'>
-      <body className={inter.className}>{children}</body>
+    <html lang="en">
+      <ThemeProvider theme={applicationTheme}>
+        <body className={inter.className}>{children}</body>
+      </ThemeProvider>
     </html>
   );
 }
