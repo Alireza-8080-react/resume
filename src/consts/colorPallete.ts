e8962f5db3a414c4fa4512898c6bc06bb0a1156e
Alireza-8080-react <Alirezashahmoradi98@gmail.com>
2023-08-24 01:04:54 +0330

    change color palette foreground to white

--- src/consts/colorPallete.ts
+++ src/consts/colorPallete.ts
@@ -1,4 +1,4 @@
 export const COLOR_PALETTE_MAIN = "#4540c2";
 export const COLOR_PALETTE_SECONDARY = "#099f01";
-export const COLOR_PALETTE_FOREGROUND = "#cfab9f";
+export const COLOR_PALETTE_FOREGROUND = "white";
 export const COLOR_PALETTE_BACKGROUND = "#060809";
