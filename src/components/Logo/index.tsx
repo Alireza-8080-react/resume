6c649b3d460cb2ff92a09d4c1370a31ec0bef0ec
Alireza-8080-react <Alirezashahmoradi98@gmail.com>
2023-08-25 10:42:04 +0330

    change logo from AlirezaShahmoradi to ASH

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,10 +1,15 @@
-import { Typography } from "@mui/material";
-import Link from "next/link";
+import { Typography } from '@mui/material';
+import Link from 'next/link';
 
 export const Logo = () => {
   return (
-    <Link href="/">
-      <Typography variant="h4">Alireza Shahmoradi</Typography>
+    <Link href='/'>
+      <Typography variant='h4' color='primary' component='span'>
+        A
+      </Typography>
+      <Typography variant='h4' component='span'>
+        SH
+      </Typography>
     </Link>
   );
 };
