f204de1b8a3fdfeb4d350ef89214387638dcdd2f
Alireza-8080-react <Alirezashahmoradi98@gmail.com>
2023-08-29 21:30:51 +0330

    sort imports of Drawer & prettier

--- src/components/Drawer/index.tsx
+++ src/components/Drawer/index.tsx
@@ -1,19 +1,14 @@
-import { Drawer as MuiDrawer, useMediaQuery, useTheme } from "@mui/material";
-import { DrawerProps } from "./models";
+import { Drawer as MuiDrawer, useMediaQuery, useTheme } from '@mui/material';
 
-export const Drawer = ({
-  isOpen,
-  onClose,
-  children,
-  title,
-  className,
-}: DrawerProps) => {
+import { DrawerProps } from './models';
+
+export const Drawer = ({ isOpen, onClose, children, title, className }: DrawerProps) => {
   const theme = useTheme();
-  const matches = useMediaQuery(theme.breakpoints.down("md"));
+  const matches = useMediaQuery(theme.breakpoints.down('md'));
 
   return (
     <MuiDrawer
-      anchor={matches ? "bottom" : "left"}
+      anchor={matches ? 'bottom' : 'left'}
       open={isOpen}
       onClose={onClose}
       title={title}
