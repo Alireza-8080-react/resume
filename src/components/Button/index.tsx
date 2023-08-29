247313ea88a069721f28d36d3e414f9c5eb549ef
Alireza-8080-react <Alirezashahmoradi98@gmail.com>
2023-08-29 21:52:20 +0330

    remove button wrapper

--- src/components/Button/index.tsx
+++
@@ -1,5 +0,0 @@
-import { Button as MuiButton, ButtonProps } from '@mui/material';
-
-export const Button = ({ children, ...props }: ButtonProps) => {
-  return <MuiButton {...props}>{children}</MuiButton>;
-};
--- src/components/Button/models.ts
+++
@@ -1,3 +0,0 @@
-import { ButtonTypeMap, ExtendButtonBase } from '@mui/material';
-
-export type ButtonProps = ExtendButtonBase<ButtonTypeMap>;
