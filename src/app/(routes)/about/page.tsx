c4256cd195f4885248e72c0c7db99ee382a39d55
Alireza <alirezashahmoradi98@gmail.com>
2023-09-06 18:04:17 +0330

    nearly empty about page

---
+++ src/app/(routes)/about/page.tsx
@@ -0,0 +1,22 @@
+import { Typography } from '@mui/material';
+
+const AboutPage = () => {
+  return (
+    <main className='body'>
+      <Typography variant='h4'>How did I start front-end?</Typography>
+      <Typography>
+        I started it by chance! I saw an HTML & CSS video on YouTube & just got curious about it. I
+        watched it till the end & followed the road map & learnt js & React & Next js & all the good
+        stuff
+      </Typography>
+      <Typography variant='h4'>How did I start front-end?</Typography>
+      <Typography>
+        I started it by chance! I saw an HTML & CSS video on YouTube & just got curious about it. I
+        watched it till the end & followed the road map & learnt js & React & Next js & all the good
+        stuff
+      </Typography>
+    </main>
+  );
+};
+
+export default AboutPage;
