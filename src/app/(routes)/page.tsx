c4f2f9e2d21a5293f07994fcae6d3d9c25eba2fc
Alireza <alirezashahmoradi98@gmail.com>
2023-09-06 16:12:45 +0330

    design left side of home page

--- src/app/(routes)/page.tsx
+++ src/app/(routes)/page.tsx
@@ -1,3 +1,32 @@
-export default function Home() {
-  return <main className="body"></main>;
-}
+import { Button, Grid, IconButton, Typography } from '@mui/material';
+import classes from './page.module.scss';
+import Link from 'next/link';
+
+const HomePage = () => {
+  return (
+    <main className='body'>
+      <Grid container>
+        <Grid item xl={4} className={classes.generalInfoContainer}>
+          <Typography variant='h2'>Mid-level Front-end Developer</Typography>
+          <Typography className={classes.caption}>
+            With 2 years of experience of React & Next JS development. Learning at highest speed
+            possible.
+          </Typography>
+          <div className={classes.contactMeButton}>
+            <IconButton>
+              <i className='bx bxl-nodejs' />
+            </IconButton>
+            <IconButton>
+              <i className='bx bxl-react' />
+            </IconButton>
+            <Link href='/contact-me'>
+              <Button variant='contained'>Contact me</Button>
+            </Link>
+          </div>
+        </Grid>
+      </Grid>
+    </main>
+  );
+};
+
+export default HomePage;
