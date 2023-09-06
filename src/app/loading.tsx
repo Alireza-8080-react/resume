f97ded5a3448cc6bdb8c1110c8e8efaca0211345
Alireza <alirezashahmoradi98@gmail.com>
2023-09-06 15:06:16 +0330

    loading page design

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,5 +1,13 @@
+import { CircularProgress } from '@mui/material';
+
+import classes from './loading.module.scss';
+
 const Loading = () => {
-  return <>Loading...</>;
+  return (
+    <div className={classes.loadingContainer}>
+      <CircularProgress size={72} />
+    </div>
+  );
 };
 
 export default Loading;
