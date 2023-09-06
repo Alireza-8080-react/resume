daf5631ad1b479ba6b504a3404500c9dc71ce2fe
Alireza <alirezashahmoradi98@gmail.com>
2023-09-07 02:25:12 +0330

    large icon sizes for social media

--- src/components/SocialMediaIcon/index.tsx
+++ src/components/SocialMediaIcon/index.tsx
@@ -6,7 +6,7 @@ export const SocialMediaIcon = ({ title, href, icon }: SocialMediaIconProps) => 
   return (
     <Tooltip title={title}>
       <Link href={href} target='_blank'>
-        <IconButton>
+        <IconButton size='large'>
           <i className={`bx bxl-${icon}`} />
         </IconButton>
       </Link>
