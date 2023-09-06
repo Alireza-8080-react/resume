7b56d67013cbe82447a95255fa84005a3ffd2d49
Alireza <alirezashahmoradi98@gmail.com>
2023-09-07 02:23:14 +0330

    fix color palette

--- src/constants/colorPalette.ts
+++ src/constants/colorPalette.ts
@@ -1,6 +1,6 @@
 export const COLOR_PALETTE_MAIN = '#4540c2';
 export const COLOR_PALETTE_SECONDARY = '#099f01';
-export const COLOR_PALETTE_FOREGROUND_DARK = '#fff';
+export const COLOR_PALETTE_FOREGROUND_DARK = '#cfab9f';
 export const COLOR_PALETTE_BACKGROUND_DARK = '#0f0f0f';
-export const COLOR_PALETTE_BACKGROUND_LIGHT = '#fff';
-export const COLOR_PALETTE_FOREGROUND_LIGHT = '#000';
+export const COLOR_PALETTE_BACKGROUND_LIGHT = '#f1f1f1';
+export const COLOR_PALETTE_FOREGROUND_LIGHT = '#242424';
