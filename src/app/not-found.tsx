43815f541ab139e6241ba4fa20b4216f0b458f59
Alireza-8080-react <Alirezashahmoradi98@gmail.com>
2023-09-08 16:23:36 +0330

    design 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,23 @@
+import { Button, Typography } from '@mui/material';
+import Image from 'next/image';
+import Link from 'next/link';
+
+import classes from './not-found.module.scss';
+
 const NotFound = () => {
-  return <>Not found...</>;
+  return (
+    <main className='body'>
+      <div className={classes.container}>
+        <Image src='404.svg' alt='404' width={350} height={350} />
+        <Typography variant='h4'>The page you are looking for is not found!</Typography>
+        <Link href='/'>
+          <Button className={classes.homePageButton} variant='contained'>
+            Home page
+          </Button>
+        </Link>
+      </div>
+    </main>
+  );
 };
 
 export default NotFound;
