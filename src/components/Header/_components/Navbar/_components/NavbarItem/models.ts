17fa38269bfa415ad66359a331fe6f279b57eab1
Alireza <alirezashahmoradi98@gmail.com>
2023-09-13 18:17:20 +0330

    enter descriptions for NavbarItemProps

--- src/components/Header/_components/Navbar/_components/NavbarItem/models.ts
+++ src/components/Header/_components/Navbar/_components/NavbarItem/models.ts
@@ -1,5 +1,14 @@
 export interface NavbarItemProps {
+  /**
+   * Title of the item
+   */
   title: string;
+  /**
+   * The link which is navigated to on click.
+   */
   link: string;
+  /**
+   * The icon which is used before text. see icons at https://boxicons.com
+   */
   icon?: string;
 }
