86f6b0710f8ce72ee8d8872887201c439b800d8f
Alireza <alirezashahmoradi98@gmail.com>
2023-09-13 16:07:32 +0330

    add description to QAAccordion

--- src/components/QAAccordion/models.ts
+++ src/components/QAAccordion/models.ts
@@ -3,7 +3,13 @@ import { ReactNode, SyntheticEvent } from 'react';
 export interface QAAccordionProps {
   isExpanded: boolean;
   onChange: (event: SyntheticEvent, isExpanded: boolean) => void;
+  /**
+   * shown in accordion summary
+   */
   question: string;
+  /**
+  The component rendered in accordion
+  */
   children: ReactNode;
   className?: string;
 }
