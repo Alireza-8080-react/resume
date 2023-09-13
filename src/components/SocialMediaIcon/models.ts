b862942e43516ba90fb60eb2ebc9a62b1c5ac554
Alireza <alirezashahmoradi98@gmail.com>
2023-09-13 16:07:42 +0330

    add descriptions for SocialMediaIcon

--- src/components/SocialMediaIcon/models.ts
+++ src/components/SocialMediaIcon/models.ts
@@ -1,5 +1,14 @@
 export interface SocialMediaIconProps {
+  /**
+   * tooltip title
+   */
   title: string;
+  /**
+   * a link which would navigate on new tab on click
+   */
   href: string;
+  /**
+   * a className which is passed to <i/> component
+   */
   iconClassName: string;
 }
