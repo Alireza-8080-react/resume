e57653903ea45bee00fd6f6e976a9fd353ef6460
Alireza-8080-react <Alirezashahmoradi98@gmail.com>
2023-09-16 08:25:16 +0330

    test

--- src/components/IconButton/index.tsx
+++ src/components/IconButton/index.tsx
@@ -1,7 +1,10 @@
 import { IconButton as MuiIconButton } from '@mui/material';
+
 import { IconButtonProps } from './models';
 
 export const IconButton = ({ size, children, onClick }: IconButtonProps) => {
+  console.log('hi');
+
   return (
     <MuiIconButton size={size} onClick={onClick}>
       {children}
