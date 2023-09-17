ebcfc5137dd048c330ed110d09bec0008cbfb51a
Alireza <alirezashahmoradi98@gmail.com>
2023-09-17 11:20:11 +0330

    change colorpalette to orange

--- src/constants/colorPalette.ts
+++ src/constants/colorPalette.ts
@@ -1,5 +1,5 @@
 export const COLOR_PALETTE_MAIN = '#4540c2';
-export const COLOR_PALETTE_SECONDARY = '#099f01';
+export const COLOR_PALETTE_SECONDARY = '#ff8400';
 export const COLOR_PALETTE_FOREGROUND_DARK = '#cfab9f';
 export const COLOR_PALETTE_BACKGROUND_DARK = '#0f0f0f';
 export const COLOR_PALETTE_BACKGROUND_LIGHT = '#f1f1f1';
