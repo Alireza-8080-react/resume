17a51f8e6fd38a18120091fa2f76aa2f5ff65261
Alireza <alirezashahmoradi98@gmail.com>
2023-09-26 20:33:06 +0330

    fix strength height problem

--- src/components/Strengths/index.tsx
+++ src/components/Strengths/index.tsx
@@ -6,7 +6,7 @@ import classes from './index.module.scss';
 export const Strengths = () => {
   return (
     <Grid container className={classes.container} justifyContent='space-around' spacing={4}>
-      <Grid item xs={10} md={3}>
+      <Grid item xs={12} md={3}>
         <Strength>
           <Typography textAlign='center'>
             I love business. So my job is not just to code. I like to know the reason behind doing
@@ -14,19 +14,19 @@ export const Strengths = () => {
           </Typography>
         </Strength>
       </Grid>
-      <Grid item xs={10} md={3}>
+      <Grid item xs={12} md={3}>
         <Strength>
           <Typography textAlign='center'>
             Super fast at doing tasks. Don't get me wrong here. I write clean code.
           </Typography>
         </Strength>
       </Grid>
-      <Grid item xs={10} md={3}>
+      <Grid item xs={12} md={3}>
         <Strength>
           <Typography textAlign='center'>Love cleaning codes.</Typography>
         </Strength>
       </Grid>
-      <Grid item xs={10} md={3}>
+      <Grid item xs={12} md={3}>
         <Strength>
           <Typography textAlign='center'>Love learning new things & growing up fast.</Typography>
         </Strength>
