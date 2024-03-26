7aeb9547e71c13aa851a2576e9e055b3eea11817
Alireza Shahmoradi <alirezashahmoradi98@gmail.com>
2024-03-26 14:38:28 +0330

    dynamic awards

---
+++ src/components/Awards/data.ts
@@ -0,0 +1,5 @@
+export const awardItems = [
+  'Spearheaded the development of a bespoke CRM interface, dynamic forms, and ticketing system, delivering a tailored front-end solution.',
+  'Integrated Mapbox GL to enable automatic user address detection, streamlining the location input process on our website.',
+  'Transformed React components to functional, integrated TypeScript & ESLint, utilized React Hook Form, and navigated complex insurance logics.'
+] as const;
--- src/components/Awards/index.tsx
+++ src/components/Awards/index.tsx
@@ -3,6 +3,7 @@ import { colorPalette } from '@/constants/colorPalette';
 import { Grid } from '../Grid';
 import { Typography } from '../Typography';
 import { Award } from './_components/Award';
+import { awardItems } from './data';
 import classes from './index.module.scss';
 
 export const Awards = () => {
@@ -15,50 +16,31 @@ export const Awards = () => {
         className={classes.inside_container}
         spacing={2}
       >
-        <Grid item xs={12} md={3}>
-          <Award
-            frontComponent={
-              <Typography color={colorPalette.light.foreground} variant='h1'>
-                #1
-              </Typography>
-            }
-            backComponent={
-              <Typography color={colorPalette.light.foreground} variant='h5' textAlign='center'>
-                I developed front-side of customized CRM, dynamic-form & ticketing panels solely.
-              </Typography>
-            }
-          />
-        </Grid>
-        <Grid item xs={12} md={3}>
-          <Award
-            frontComponent={
-              <Typography color={colorPalette.light.foreground} variant='h1'>
-                #2
-              </Typography>
-            }
-            backComponent={
-              <Typography color={colorPalette.light.foreground} variant='h5' textAlign='center'>
-                Added location to website to get users address by using Mapbox gl.
-              </Typography>
-            }
-          />
-        </Grid>
-        <Grid item xs={12} md={3}>
-          <Award
-            frontComponent={
-              <Typography color={colorPalette.light.foreground} variant='h1'>
-                #3
-              </Typography>
-            }
-            backComponent={
-              <Typography color={colorPalette.light.foreground} variant='h5' textAlign='center'>
-                Refactored many areas of project: Convert class components into functional Add ts &
-                es-lint with custom strict rules Used React-hook-form for forms As insurance has
-                many if-elses (logics) I learnt how to handle many scenarios.
-              </Typography>
-            }
-          />
-        </Grid>
+        {awardItems.map((awardItem, index) => (
+          <Grid item xs={12} md={3} key={index}>
+            <Award
+              frontComponent={
+                <Typography color={colorPalette.light.foreground} variant='h1'>
+                  #{index + 1}
+                </Typography>
+              }
+              backComponent={
+                <Grid container direction='column' spacing={2} className={classes.back}>
+                  <Grid item>
+                    <Typography color={colorPalette.light.foreground} variant='h3'>
+                      #{index + 1}
+                    </Typography>
+                  </Grid>
+                  <Grid item>
+                    <Typography color={colorPalette.light.foreground} variant='body1'>
+                      {awardItem}
+                    </Typography>
+                  </Grid>
+                </Grid>
+              }
+            />
+          </Grid>
+        ))}
       </Grid>
     </div>
   );
