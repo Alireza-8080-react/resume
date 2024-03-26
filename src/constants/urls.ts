d7224b0aae863b39172ec8601c0a74e5e96039c6
Alireza Shahmoradi <alirezashahmoradi98@gmail.com>
2024-03-26 14:39:14 +0330

    change gmail link

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -2,7 +2,6 @@ export const urls = {
   azki: 'https://azki.com',
   cv: 'https://docs.google.com/document/d/1JmfdXPb-iRSWpYF4pg-ElMdvuRqj9-Nsp0QpF68PzCM/edit?usp=sharing',
   githubRepo: 'https://github.com/Alireza-8080-react/resume',
-  gmail:
-    'https://mail.google.com/mail/u/0/?ogbl#inbox?compose=CllgCJTJnslxZFCNZNxfqMZjXlsrDVkPvRWTxVqKSWlpzLbsMMwDqfXvHZVPMNNWsqHwTjLnjpL',
+  gmail: 'mailto:alireza.shahmoradi.entrepreneur@gmail.com',
   linkedIn: 'https://www.linkedin.com/in/alireza-shahmoradi/'
 } as const;
