147e2c5499f172c2937fd888067c3c21349b48dc
Alireza Shahmoradi <alirezashahmoradi98@gmail.com>
2024-03-26 14:39:35 +0330

    remove extra div from SocialMediaIcon

--- src/components/SocialMediaIcon/index.tsx
+++ src/components/SocialMediaIcon/index.tsx
@@ -6,12 +6,12 @@ import { SocialMediaIconProps } from './models';
 
 export const SocialMediaIcon = ({ title, href, children }: SocialMediaIconProps) => {
   return (
-    <Tooltip title={title}>
-      <Link href={href}>
-        <IconButton size='large'>
-          <div className={classes.icon}>{children}</div>
+    <Link href={href}>
+      <Tooltip title={title}>
+        <IconButton size='large' className={classes.icon}>
+          {children}
         </IconButton>
-      </Link>
-    </Tooltip>
+      </Tooltip>
+    </Link>
   );
 };
